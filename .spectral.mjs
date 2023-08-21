444173cdba61a4e4ab4f750c400e6c08ed4398b2
takumidiary.0927@gmail.com <takumidiary.0927@gmail.com>
2023-08-21 22:12:01 +0900

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIxNTA1Ng/spectral.js?branch_slug=main&token=3cbd19a0-c2b7-426a-8f93-3ebee064e6ae";
+export default { extends: ruleset };
